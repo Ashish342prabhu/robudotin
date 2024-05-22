b210b6075326f629ff6b96b98a535e1881c8962e
dhanushbd03 <dhanushrocks03@gmail.com>
2024-05-22 22:10:26 +0530

    correction

--- robudotin/settings.py
+++ robudotin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-htw#*vno^h!22!0(hnz$kza(+7)$d3v4hk*cs&an@1c7e^=@mh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.1.34','*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build",'static' ) / 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "theme", "static"),
